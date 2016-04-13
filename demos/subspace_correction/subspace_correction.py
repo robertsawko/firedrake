df38ccc8da9224b8cdd05a5af4c3eee2c71946bf
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-04-13 10:19:44 +0100

    Construct mask for patch boundaries
    We apply strong, homogeneous, boundary conditions to all dofs on the
    boundary of patches.  We do not include facets on the boundary of
    the *domain* as being on the boundary of patches.  If strong (Dirichlet)
    conditions are supplied to the bilinear form, then these are used to
    kill dofs on the domain boundary, otherwise we assume there are
    Neumann-like conditions (and so the dofs should not be killed).

--- demos/subspace_correction/subspace_correction.py
+++ demos/subspace_correction/subspace_correction.py
@@ -2,6 +2,7 @@ from firedrake import *
 import numpy
 import collections
 import itertools
+import functools
 
 
 class SubspaceCorrectionPrec(object):
@@ -12,10 +13,20 @@ class SubspaceCorrectionPrec(object):
     patch together with a global low-order discretization.
 
     :arg a:  A bilinear form defined in UFL
+    :arg bcs: Optional strongly enforced boundary conditions
     """
-    
-    def __init__(self, a):
+    def __init__(self, a, bcs=None):
         self.a = a
+        if bcs is None:
+            bcs = ()
+        try:
+            bcs = tuple(bcs)
+        except TypeError:
+            bcs = (bcs, )
+        nodes = set()
+        for bc in bcs:
+            nodes.update(bc.nodes)
+        self.bc_nodes = nodes
 
         # one phase of the preconditioner involves restricting the problem
         # to the patch around each vertex and solving it.  So, we'll need
@@ -52,14 +63,14 @@ class SubspaceCorrectionPrec(object):
             for c in cells:
                 faces = dm.getCone(c)
                 for f in faces:
+                    # Only select faces if they are not on the domain boundary
+                    if dm.getLabelValue("exterior_facets", f) == 1:
+                        continue
                     closure, _ = dm.getTransitiveClosure(f, useCone=True)
                     if v not in closure:
                         boundary_faces.append(f)
             patch_faces.append(boundary_faces)
             # Both of the vertices and cells are in plex numbering,
-
-            firedrake_vertex = vtx_numbering.getOffset(v)
-
             patches.append(numpy.array([cell_numbering.getOffset(c)
                                         for c in cells]))
 
@@ -69,42 +80,49 @@ class SubspaceCorrectionPrec(object):
 
         dof_patches = []
         glob_patches = []
+        bc_masks = []
         from functools import partial
-        for patch, faces  in zip(patches, patch_faces):
+        for patch, faces in zip(patches, patch_faces):
             local = collections.defaultdict(partial(next, itertools.count()))
             dof_patch = numpy.empty((len(patch), cell_node_map.shape[-1]),
                                     dtype=int)
+            bc_mask = numpy.zeros(dof_patch.shape, dtype=bool)
             for i, c in enumerate(patch):
                 for j, dof in enumerate(cell_node_map[c, :]):
                     dof_patch[i, j] = local[dof]
+                    # Mask out global dirichlet bcs
+                    if dof in self.bc_nodes:
+                        bc_mask[i, j] = True
             glob_patch = numpy.empty(dof_patch.max() + 1, dtype=int)
             for i, j in numpy.ndindex(dof_patch.shape):
                 glob_patch[dof_patch[i, j]] = cell_node_map[patch[i], j]
 
+            # Mask out dofs on boundary of patch
             for f in faces:
                 closure, _ = dm.getTransitiveClosure(f, useCone=True)
                 for p in closure:
                     off = dof_section.getOffset(p)
                     for j in range(dof_section.getDof(p)):
-                        glob_index = np.where(glob_patch == off + j)
-                for j in range(dof_section.getDof(f)):
+                        bc_mask[numpy.where(dof_patch == local[off + j])] = True
+
             glob_patches.append(glob_patch)
             dof_patches.append(dof_patch)
+            bc_masks.append(bc_mask)
 
-            
-
-        for p, d, g in zip(patches, dof_patches, glob_patches):
+        for p, d, g, bcs in zip(patches, dof_patches, glob_patches, bc_masks):
             print p
             print d
             print g
+            print bcs
             print
         
         
         
 M = RectangleMesh(2, 2, 1.0, 1.0)
 V = FunctionSpace(M, "CG", 2)
+bcs = DirichletBC(V, 0, (1, 2, 3, 4))
 u = TrialFunction(V)
 v = TestFunction(V)
 a = inner(grad(u), grad(v))*dx
 
-SCP = SubspaceCorrectionPrec(a)
+SCP = SubspaceCorrectionPrec(a, bcs=None)
